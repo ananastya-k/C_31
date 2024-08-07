c7c22131195cd39f575cd561d22cad675116bbaf
dudzich <nastyakrupovich1998@gmail.com>
2024-08-07 16:36:42 +0300

    Lesson 3: Add func with task 5

--- src/Lesson3.java
+++ src/Lesson3.java
@@ -18,6 +18,14 @@ public class Lesson3 {
         System.out.println("\nTask 4:");
         executeTask4();
 
+        System.out.print("\nTask 5. \nInput number: ");
+        int nextInt;
+        if(sc.hasNextInt() && (nextInt=sc.nextInt()) > 0){
+            System.out.println(executeTask5(nextInt));
+        } else {
+            System.out.println("Incorrect input");
+        }
+
     }
 
     // return true if num is Even, else - false
@@ -41,11 +49,21 @@ public class Lesson3 {
         System.out.println();
     }
 
-    // Prints multiples of 7 from 7 to 98.
+    // Prints multiples of 7 from 7 to 98
     public static void executeTask4() {
-        for (int i = 7; i < 100; i+=7){
+        for (int i = 7; i < 100; i += 7){
             System.out.print(i + " ");
         }
         System.out.println();
     }
+
+    // Calculates the sum of integers from 1 to n
+    public static int executeTask5(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i ++){
+            sum += i;
+        }
+        return sum;
+    }
+
 }
