d1487e55705ae04a56729983b1dc4d901b85ff95
dudzich <nastyakrupovich1998@gmail.com>
2024-08-07 16:22:50 +0300

    Lesson 3: Add func with task 3

--- src/Lesson3.java
+++ src/Lesson3.java
@@ -12,6 +12,9 @@ public class Lesson3 {
         System.out.print("\nTask 2.\nInput temperature: ");
         System.out.println(executeTask2(sc.nextInt()));
 
+        System.out.println("\nTask 3:\n");
+        executeTask3();
+
     }
 
     // return true if num is Even, else - false
@@ -27,4 +30,10 @@ public class Lesson3 {
                 ? "Normal" : "Cold";
     }
 
+    // Prints the squares of integers from 10 to 20
+    public static void executeTask3() {
+        for (int i = 10; i <= 20; i++){
+            System.out.print(i * i + " ");
+        }
+    }
 }
