814766d022a099e26f46d9a09d0a9d5b904ab7a4
dudzich <nastyakrupovich1998@gmail.com>
2024-08-07 16:17:38 +0300

    Lesson 3: Add func with task 2

--- src/Lesson3.java
+++ src/Lesson3.java
@@ -9,11 +9,22 @@ public class Lesson3 {
         System.out.print("\nTask 1.\nInput number: ");
         System.out.println(executeTask1(sc.nextInt()));
 
+        System.out.print("\nTask 2.\nInput temperature: ");
+        System.out.println(executeTask2(sc.nextInt()));
+
     }
 
     // return true if num is Even, else - false
     public static boolean executeTask1(int n) {
         return n % 2 == 0;
+    }
+
+
+    // Returns "Warm" if n > -5, "Normal" if n > -20, otherwise "Cold"
+    public static String executeTask2(int n) {
+        return n > -5
+                ? "Warm" : n >-20
+                ? "Normal" : "Cold";
     }
 
 }
